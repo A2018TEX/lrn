a0312a88b0a50c80aa08e556aba0fcfb78608ec4
Olga <olga16vk@gmail.com>
2019-12-10 18:37:38 +0200

    fix solution

---
+++ ConfigurationFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Laren.E2ETests
+{
+    public class ConfigurationFactory
+    {
+        public IConfiguration CreateInstance()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Qa";
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+            .AddEnvironmentVariables();
+            IConfiguration configuration = builder.Build();
+            return configuration;
+        }
+    }
+}
